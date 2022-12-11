ec805cd42a535e72ffba73e8db0e515dc723e34a
Jeremy Pittard <jeremy@jpittard.net>
2022-12-11 15:47:41 +0800

    free

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -2,7 +2,7 @@ const Intro = () => {
   return (
     <div>
       <p className="px-4 mt-6 text-xl">
-        {`Introducing Good Brief - a weekly newsletter filled with positive and
+        {`Introducing Good Brief - a free weekly newsletter filled with positive and
         uplifting news stories from the past week! In a world that can sometimes
         feel overwhelming, it's important to take a step back and focus on the
         good. Let Good Brief be your source for all the heartwarming, inspiring,
