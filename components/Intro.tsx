31bb0108f96df8c4569d8426db7ea98984dad8ec
Jeremy Pittard <jeremy@jpittard.net>
2023-03-21 10:40:12 +0800

    removed launching copy

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -6,8 +6,10 @@ const Intro = () => {
         uplifting news stories from the past week! In a world that can sometimes
         feel overwhelming, it's important to take a step back and focus on the
         good. Let Good Brief be your source for all the heartwarming, inspiring,
-        and feel-good news in bite-sized portions. Sign up now to start your
-        week off on the right foot. Launching early 2023.`}
+        and feel-good news in bite-sized portions. `}
+      </p>
+      <p className="px-4 mt-6 text-xl">
+        {`Sign up now to start your week off on the right foot.`}
       </p>
     </div>
   );
